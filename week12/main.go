4a71c02be6c2efe740abb43abc4ee481c2ad8054
용다인 <cccohdlakk339@naver.com>
2023-11-13 17:15:01 +0900

    week12 v3.0

--- week12/main.go
+++ week12/main.go
@@ -19,7 +19,14 @@ func main() {
 	}
 
 	test := [3]string{"inha", "go", "student"}
-	testS := test[0:2] // testS := test[0:2]
+	testS := test[:2] // testS := test[0:2]
+	testS2 := test[1:]
+
+	// testS2[0] = "python"
+	// test[1] = "python"
+	testS[1] = "python"
+
 	fmt.Println(test, len(test))
 	fmt.Println(testS, len(testS))
+	fmt.Println(testS2, len(testS2))
 }
