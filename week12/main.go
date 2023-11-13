6135ded8d3d5fce8a46f8261aacd99aa454ee140
용다인 <cccohdlakk339@naver.com>
2023-11-13 17:51:44 +0900

    week12 v4.1

--- week12/main.go
+++ week12/main.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-	a := []string{"a", "b", "c", "d"}
-	as := a[:2]
-	as[1] = "Z"
-	fmt.Println(a, as)
+// import "fmt"
 
-	b := [4]int{4, 3, 2, 1}
-	bs := b[1:3]
-	fmt.Println(b, bs)
+func main() {
+	a := make([]string, 4, 5) // type, length, capacity(수용량)
+	a[0] = "a"
+	a[2] = "c"
+	a[3] = "d"
+	as := a[0:2]
+	as[1] = "Z"
+	c := append(a, "y")
+	// c := append(a, "y", "x")
+
+	c[0] = "q"
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
 }
+
+// func main() {
+// 	a := []string{"a", "b", "c", "d"}
+// 	as := a[:2]
+// 	as[1] = "Z"
+// 	fmt.Println(a, as)
+
+// 	b := [4]int{4, 3, 2, 1}
+// 	bs := b[1:3]
+// 	fmt.Println(b, bs)
+// }
 
 // package main
 
